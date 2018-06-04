858ab69deb24ce6432dfb04c66e83fd626f38f6d
Marlene Weiss <marlene.weiss@student.uibk.ac.at>
2018-06-04 17:50:21 +0200

    Versuch, Plugin Elevation in Wanderkarte einzubauen

--- sport/wandern.js
+++ sport/wandern.js
@@ -4,7 +4,7 @@ let myMap = L.map("mapdiv", {
         position: 'topleft'
     }
 });
-
+let overlayElevation = L.featureGroup().addTo(myMap);
 //DOClink: 1.3.0.html#map-l-map
 let myLayers= {
     osm: L.tileLayer (
@@ -44,9 +44,6 @@ let ortho_m_beschr = L.featureGroup (
     myLayers.tiris_nomenklatur]
 );
 
-
-myMap.addLayer(myLayers.osm);
-
 //DOCLINK: http://leafletjs.com/reference-1.3.0.html#map-addlayer
 
 //DOKLINK: http://leafletjs.com/reference-1.3.0.html#control-layers-l-control-layers
@@ -82,7 +79,6 @@ L.control.scale({
         position: "bottomleft" //DOCLINK: http://leafletjs.com/reference-1.3.0.html#control-scale-position
 }).addTo(myMap);
 
-
 let gpxTrack = new L.GPX ("AdlerwegEtappe12.gpx", {
     async: true,
 });
@@ -91,6 +87,38 @@ gpxTrack.on ("loaded", function (evt) {
     myMap.fitBounds(track.getBounds());
 }).addTo(myMap); 
 
+let hoehenProfil = L.control.elevation({
+    position: "topright",
+  theme: "lime-theme", //default: lime-theme
+  width: 600,
+  height: 125,
+  margins: {
+      top: 10,
+      right: 20,
+      bottom: 30,
+      left: 50
+  },
+  useHeightIndicator: true, //if false a marker is drawn at map position
+  interpolation: "linear", //see https://github.com/mbostock/d3/wiki/SVG-Shapes#wiki-area_interpolate
+  hoverNumber: {
+      decimalsX: 3, //decimals on distance (always in km)
+      decimalsY: 0, //deciamls on hehttps://www.npmjs.com/package/leaflet.coordinatesight (always in m)
+      formatter: undefined //custom formatter function may be injected
+  },
+  xTicks: undefined, //number of ticks in x axis, calculated by default according to width
+  yTicks: undefined, //number of ticks on y axis, calculated by default according to height
+  collapsed: true,  //collapsed mode, show chart on click or mouseover
+  imperial: false    //display imperial units instead of metric
+});
 
+hoehenProfil.addTo(myMap);
+
+gpxTrack.on("addline",function(evt){
+    hoehenProfil.addData(evt.line);
+})
+/*L.geoJson(geojson,{
+    onEachFeature: hoehenProfil.addData.bind(hoehenProfil) //working on a better solution
+}).addTo(myMap);
+*/
 
 
