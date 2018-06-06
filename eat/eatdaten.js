e82609832a517a3c135e76a8dbea53f8266c15d6
Jacob Heuser <jacob.heuser@student.uibk.ac.at>
2018-06-06 21:27:10 +0200

    eatdaten

---
+++ eat/eatdaten.js
@@ -0,0 +1,24 @@
+const eatdaten=[{
+    "lat":"47.266463","lng":"11.384141","name":"Alia's","Art":"Falafel-Bistro","Internetseite":"<a href=http://alias.business.site/>Internetseite</a>","Öffnungszeiten":"<ul><li>montag:11.30-15.00 & 17.00-20.00</li><li>dienstag:11.30-15.00</li><li>mittwoch:11.30-15.00 & 17.00-20.00</li><li>donnerstag:11.30-15.00 & 17.00-20.00</li><li>freitag:11.30-15.00 & 17.00-20.00</li><li>samstag:11.30-15.00</li></ul>","Infos":"<p>Die Besten Falafel der Stadt!</p>"
+},{
+    "lat":"47.267775","lng":"11.396384","name":"Treibhaus","Art":"#","Internetseite":"","Öffnungszeiten":"#","Infos":"#"
+},{
+    "lat":"47.269184","lng":"11.397918","name":"Himal","Art":"#","Internetseite":"","Öffnungszeiten":"#","Infos":"#"
+},{
+    "lat":"47.264296","lng":"11.389632","name":"Machete","Art":"#","Internetseite":"","Öffnungszeiten":"#","Infos":"#"
+},{
+    "lat":"47.268891","lng":"11.396784","name":"Cafè Elektra","Art":"Griechisch-Österreichisches Kulturcafè","Internetseite":"","Öffnungszeiten":"#","Infos":"#"
+},{
+    "lat":"47.264799","lng":"11.397920","name":"Pizzerei","Art":"#","Internetseite":"","Öffnungszeiten":"#","Infos":"#"
+},{
+    "lat":"47.269430","lng":"11.393107","name":"Cafè Bar Moustache","Art":"#","Internetseite":"","Öffnungszeiten":"#","Infos":"#"
+},{
+    "lat":"47.261750","lng":"11.395234","name":"Kater Noster - Cafè Bar","Art":"#","Internetseite":"","Öffnungszeiten":"#","Infos":"#"
+},{
+    "lat":"47.269152","lng":"11.389636","name":"John Montagu","Art":"#","Internetseite":"","Öffnungszeiten":"#","Infos":"#"
+},{
+    "lat":"47.268941","lng":"11.404886","name":"Die Bäckerei","Art":"Kulturbackstube","Internetseite":"","Öffnungszeiten":"#","Infos":"#"
+},{
+    "lat":"#","lng":"#","name":"#","Art":"#","Internetseite":"","Öffnungszeiten":"#","Infos":"#"
+}]
+
