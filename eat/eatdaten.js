8f63d47553be2c7b17b454c0b3f1bd8649222419
Jacob Heuser <jacob.heuser@student.uibk.ac.at>
2018-06-07 22:41:48 +0200

    eat+favicon

--- eat/eatdaten.js
+++ eat/eatdaten.js
@@ -1,26 +1,38 @@
 const eatdaten=[{
-    "lat":"47.266463","lng":"11.384141","name":"Alia's","Art":"Falafel-Bistro","Internetseite":"<a href=http://alias.business.site/>Internetseite</a>","Öffnungszeiten":"<h3>Öffnungszeiten:</h3><ul><li>montag:11.30-15.00 & 17.00-20.00</li><li>dienstag:11.30-15.00</li><li>mittwoch:11.30-15.00 & 17.00-20.00</li><li>donnerstag:11.30-15.00 & 17.00-20.00</li><li>freitag:11.30-15.00 & 17.00-20.00</li><li>samstag:11.30-15.00</li></ul>","Infos":"<p>Die Besten Falafel der Stadt!</p>"
+    "lat":"47.266463","lng":"11.384141","name":"Alia's","Art":"Falafel-Bistro","Internetseite":"<a href=http://alias.business.site/>Internetseite</a>","Öffnungszeiten":"<h3>Öffnungszeiten</h3><ul><li>montag:11.30-15.00 & 17.00-20.00</li><li>dienstag:11.30-15.00</li><li>mittwoch:11.30-15.00 & 17.00-20.00</li><li>donnerstag:11.30-15.00 & 17.00-20.00</li><li>freitag:11.30-15.00 & 17.00-20.00</li><li>samstag:11.30-15.00</li></ul>","Infos":"<p>Die Besten Falafel der Stadt!</p>"
 },{
     "lat":"47.267775","lng":"11.396384","name":"Treibhaus","Art":"#","Internetseite":"","Öffnungszeiten":"#","Infos":"#"
 },{
-    "lat":"47.269184","lng":"11.397918","name":"Himal","Art":"#","Internetseite":"","Öffnungszeiten":"#","Infos":"#"
+    "lat":"47.269184","lng":"11.397918","name":"Himal","Art":"Nepalesische Küche","Internetseite":"<a href=http://himal.at/>Wesite</a>","Öffnungszeiten":"<h3>Öffnungszeiten</h3><ul><li>Mo – Do 11.45 - 14.30 Uhr und 18.00 - 22.00 Uhr</li><li>Fr – Sa 11.45 - 14.30 Uhr und 18.00 - 22.15 Uhr</li><li>So- und Feiertage 18.00 - 22.00 Uhr</li></ul><p>Reservierung: +43 5352 629 43</p>","Infos":"Sehr zu empfehlen - auch für den kleineren Geldbeutel!"
 },{
-    "lat":"47.264296","lng":"11.389632","name":"Machete","Art":"#","Internetseite":"","Öffnungszeiten":"#","Infos":"#"
+    "lat":"47.264296","lng":"11.389632","name":"Machete","Art":"Burrito Kartell","Internetseite":"<a href=http://www.machete-burritos.com/>Wesite</a>","Öffnungszeiten":"<h3>Öffnungszeiten</h3><ul><li>Dienstag – Donnerstag  11:30 - 01:00 Uhr</li><li>Freitag & Samstag 11:30 - 02:00 Uhr</li><li>Sonntag 11:30 - 24:00 Uhr</li><li>Küche: täglich außer Montag 11:30 - 22.00 Uhr</li></ul>","Infos":"Wir konzentrieren uns auf die wichtigen Dinge des Lebens. Burritos. Am Abend wird allerdings auch des Öfteren der Burrito zur Seite gelegt und das Tegernseer oder ein Moscow Mule in die Hand genommen. Dann lädt das Kartell Nachtschwärmer zum gemütlichen Verweilen bei Craftbier, Cocktails und anderen fancy schmancy Drinks."
 },{
-    "lat":"47.268891","lng":"11.396784","name":"Cafè Elektra","Art":"Griechisch-Österreichisches Kulturcafè","Internetseite":"","Öffnungszeiten":"#","Infos":"#"
+    "lat":"47.268891","lng":"11.396784","name":"Cafè Elektra","Art":"Griechisch-Österreichisches Kulturcafè","Internetseite":"<a href=><a/>","Öffnungszeiten":"<h3>Öffnungszeiten</h3>","Infos":"#"
 },{
-    "lat":"47.264799","lng":"11.397920","name":"Pizzerei","Art":"#","Internetseite":"","Öffnungszeiten":"#","Infos":"#"
+    "lat":"47.264799","lng":"11.397920","name":"Pizzerei","Art":"Pizzeria","Internetseite":"<a href=><a/>","Öffnungszeiten":"<h3>Öffnungszeiten</h3>","Infos":"#"
 },{
-    "lat":"47.269430","lng":"11.393107","name":"Cafè Bar Moustache","Art":"#","Internetseite":"","Öffnungszeiten":"#","Infos":"#"
+    "lat":"47.269430","lng":"11.393107","name":"Cafè Bar Moustache","Art":"#","Internetseite":"<a href=><a/>","Öffnungszeiten":"<h3>Öffnungszeiten</h3>","Infos":"#"
 },{
-    "lat":"47.261750","lng":"11.395234","name":"Kater Noster - Cafè Bar","Art":"#","Internetseite":"","Öffnungszeiten":"#","Infos":"#"
+    "lat":"47.261750","lng":"11.395234","name":"Kater Noster - Cafè Bar","Art":"#","Internetseite":"<a href=><a/>","Öffnungszeiten":"<h3>Öffnungszeiten</h3>","Infos":"#"
 },{
-    "lat":"47.269152","lng":"11.389636","name":"John Montagu","Art":"#","Internetseite":"","Öffnungszeiten":"#","Infos":"#"
+    "lat":"47.269152","lng":"11.389636","name":"John Montagu","Art":"#","Internetseite":"<a href=><a/>","Öffnungszeiten":"<h3>Öffnungszeiten</h3>","Infos":"#"
 },{
-    "lat":"47.268941","lng":"11.404886","name":"Die Bäckerei","Art":"Kulturbackstube","Internetseite":"","Öffnungszeiten":"#","Infos":"#"
+    "lat":"47.268941","lng":"11.404886","name":"Die Bäckerei","Art":"Kulturbackstube","Internetseite":"<a href=><a/>","Öffnungszeiten":"<h3>Öffnungszeiten</h3>","Infos":"#"
 },{
     "lat":"47.263154","lng":"11.398662","name":"Celi's Impro-Bistro","Art":"Zauberküche","Öffnungszeiten":"Geheim","Infos":"Regelmäßig wird Sonntag Abends ein kleines Fest gefeiert..."
 },{
-    "lat":"47.264121","lng":"11.395240","name":"Crocodilles Pizza","Art":"Pizzeria & Lieferservice","Internetseite":"<a href=http://www.pizza-crocodiles.at/>Crocodilles Website</a>","Öffnungszeiten":"<h2>Öffnungszeiten</h2><p>montag bis samstag: 11-23 Uhr</p>","Infos":"Montag ist Schontag - Zwischen 20 und 23 Uhr lad das Crocodilles herzlichst zu günstigen Pizzaabenden ein.(nur gültig bei zwei Bestellungen aus der Karte)"
+    "lat":"47.264121","lng":"11.395240","name":"Crocodilles Pizza","Art":"Pizzeria & Lieferservice","Internetseite":"<a href=http://www.pizza-crocodiles.at/>Crocodilles Website</a>","Öffnungszeiten":"<h3>Öffnungszeiten</h3><p>montag bis samstag: 11-23 Uhr</p>","Infos":"Montag ist Schontag - Zwischen 20 und 23 Uhr lad das Crocodilles herzlichst zu günstigen Pizzaabenden ein.(nur gültig bei zwei Bestellungen aus der Karte)"
+},{
+    "lat":"47.271804","lng":"11.405228","name":"Due Sicilie A CASA","Art":"Pizzeria","Internetseite":"<a href=http://www.duesicilie.at/>Website</a>","Öffnungszeiten":"<h3>Öffnungszeiten</h3>","Infos":"Die Beste Pizza der Stadt..?!"
+},{
+    "lat":"47.255727","lng":"11.397442","name":"Riese Haymon","Art":"Traditionelle Tiroler Küche","Internetseite":"<a href=https://www.riese-haymon.at/>Website</a>","Öffnungszeiten":"<h3>Öffnungszeiten</h3><p>Dienstag - Samstag: 10.30-23.30 Uhr</p>","Infos":"Der Gast kommt als Fremder, und geht als Freund des Hauses, denn Sie lieber Gast, sind das Wertvollste was wir haben, und wir freuen uns auf ein Wiederseh`n! Die Wirtsleute, Andrea und Fritz Marx"
 }]
+
+
+	
+ 
+	
+
+
+
 
