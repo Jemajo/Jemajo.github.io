eea574c65ee1996d2fbc0079b41a67ce2afd5c32
marlene weiss <marlene.weiss@stud.uibk.ac.at>
2018-06-07 11:53:34 +0200

    Wandern Etappe 13 hinzugefügt, Popups entfernt

--- sport/wandern.js
+++ sport/wandern.js
@@ -5,7 +5,9 @@ let myMap = L.map("mapdiv", {
         position: 'topleft'
     }
 });
-let etappeGroup = L.featureGroup(); 
+
+let etappeGroup12 = L.featureGroup(); 
+let etappeGroup13 = L.featureGroup();
 //DOClink: 1.3.0.html#map-l-map
 let myLayers= {
     osm: L.tileLayer (
@@ -46,17 +48,121 @@ let ortho_m_beschr = L.featureGroup (
     myLayers.tiris_nomenklatur]
 );
 
+//Marker-Layer Etappe 12
+myMap.addLayer(etappeGroup12);
+const start12 = [47.35467730, 11.47713480];
+const finish12 = [47.27091590, 11.39537570];
+
+L.marker(start12, {icon: L.icon({iconUrl: '../icons/start.png', iconAnchor: [15, 35],}) 
+}).addTo(etappeGroup12); 
+
+L.marker(finish12, {icon: L.icon({iconUrl: '../icons/finish.png', iconAnchor: [15, 35],}) 
+}).addTo(etappeGroup12);
+
+//Marker-Layer Etappe 13
+myMap.addLayer(etappeGroup13);
+const start13 = [47.21111820, 11.45217410];
+const finish13 = [47.22586360, 11.52931450];
+
+L.marker(start13, {icon: L.icon({iconUrl: '../icons/start.png', iconAnchor: [15, 35],}) 
+}).addTo(etappeGroup13); 
+
+L.marker(finish13, {icon: L.icon({iconUrl: '../icons/finish.png', iconAnchor: [15, 35],}) 
+}).addTo(etappeGroup13);
+
+// Etappe 12
+let gpxTrack12 = new L.GPX ("AdlerwegEtappe12.gpx", {
+    async: true,
+});
+
+gpxTrack12.on ("loaded", function (evt) {
+//    {style: function (feature) {return {color: "#b40000"}};
+    let track = evt.target;
+}).addTo(myMap); 
+
+let hoehenProfil12 = L.control.elevation({
+    position: "topright",
+  theme: "lime-theme", //default: lime-theme
+  width: 600,
+  height: 125,
+  margins: {
+      top: 10,
+      right: 20,
+      bottom: 30,
+      left: 50
+  },
+  useHeightIndicator: true,
+  interpolation: "linear", //see https://github.com/mbostock/d3/wiki/SVG-Shapes#wiki-area_interpolate
+  hoverNumber: {
+      decimalsX: 3, 
+      decimalsY: 0, 
+      formatter: undefined 
+  },
+  xTicks: undefined, 
+  yTicks: undefined, 
+  collapsed: true,  
+  imperial: false
+}).addTo(myMap);
+
+gpxTrack12.on("addline",function(evt){
+   hoehenProfil12.addData(evt.line);
+});
+gpxTrack12.addTo(myMap); 
+
+// Etappe 13
+let gpxTrack13 = new L.GPX ("AdlerwegEtappe13.gpx", {
+    async: true,
+});
+
+gpxTrack13.on ("loaded", function (evt) {
+//    {style: function (feature) {return {color: "#b40000"}};
+    let track = evt.target;
+}).addTo(myMap); 
+
+let hoehenProfil = L.control.elevation({
+    position: "topright",
+  theme: "lime-theme", //default: lime-theme
+  width: 600,
+  height: 125,
+  margins: {
+      top: 10,
+      right: 20,
+      bottom: 30,
+      left: 50
+  },
+  useHeightIndicator: true,
+  interpolation: "linear", //see https://github.com/mbostock/d3/wiki/SVG-Shapes#wiki-area_interpolate
+  hoverNumber: {
+      decimalsX: 3, 
+      decimalsY: 0, 
+      formatter: undefined 
+  },
+  xTicks: undefined, 
+  yTicks: undefined, 
+  collapsed: true,  
+  imperial: false
+}).addTo(myMap);
+
+gpxTrack13.on("addline",function(evt){
+   hoehenProfil.addData(evt.line);
+});
+gpxTrack13.addTo(myMap); 
+
 //DOCLINK: http://leafletjs.com/reference-1.3.0.html#map-addlayer
 
 //DOKLINK: http://leafletjs.com/reference-1.3.0.html#control-layers-l-control-layers
 let myMapControl = L.control.layers ({
-    //<Object> baselayers?
+
     "Karte" :myLayers.osm,
     "Orthofoto":ortho_m_beschr,
     
 },
 { 
-    "Start- & Endpunkt der Tour" : etappeGroup,
+    "Start- & Endpunkt der Etappe 12" : etappeGroup12, 
+    "Track der Etappe 12" : gpxTrack12,
+    "Start- & Endpunkt der Etappe 13" : etappeGroup13,
+    "Track der Etappe 13" : gpxTrack13,
+
 },
 {
     collapsed: false
@@ -79,61 +185,13 @@ L.control.scale({
         position: "bottomleft" //DOCLINK: http://leafletjs.com/reference-1.3.0.html#control-scale-position
 }).addTo(myMap);
 
+// Gruppierung für fitBounds
+let etappeGroups = L.featureGroup(
+    [
+        etappeGroup12, 
+        etappeGroup13,
+    ]
+);
 
-myMap.addLayer(etappeGroup);
-const start = [47.27091590, 11.39537570];
-const finish = [47.35467730, 11.47713480];
-
-L.marker(start, {icon: L.icon({iconUrl: '../icons/start.png', iconAnchor: [15, 35],}) 
-}).addTo(etappeGroup).bindPopup("<p>Startpunkt</p><a href='https://de.wikipedia.org/wiki/St._Anton_am_Arlberg'>Wikipedia Link</a>"); 
-
-L.marker(finish, {icon: L.icon({iconUrl: '../icons/finish.png', iconAnchor: [15, 35],}) 
-}).addTo(etappeGroup).bindPopup("<p>Endpunkt</p><a href='https://de.wikipedia.org/wiki/Steeg_(Tirol)'>Wikipedia Link</a>");
-
-let gpxTrack = new L.GPX ("AdlerwegEtappe12.gpx", {
-    async: true,
-});
-
-gpxTrack.on ("loaded", function (evt) {
-//    {style: function (feature) {return {color: "#b40000"}};
-    let track = evt.target;
-    myMap.fitBounds(track.getBounds());
-}).addTo(myMap); 
-
-let hoehenProfil = L.control.elevation({
-    position: "topright",
-  theme: "lime-theme", //default: lime-theme
-  width: 600,
-  height: 125,
-  margins: {
-      top: 10,
-      right: 20,
-      bottom: 30,
-      left: 50
-  },
-  useHeightIndicator: true, //if false a marker is drawn at map position
-  interpolation: "linear", //see https://github.com/mbostock/d3/wiki/SVG-Shapes#wiki-area_interpolate
-  hoverNumber: {
-      decimalsX: 3, //decimals on distance (always in km)
-      decimalsY: 0, //deciamls on hehttps://www.npmjs.com/package/leaflet.coordinatesight (always in m)
-      formatter: undefined //custom formatter function may be injected
-  },
-  xTicks: undefined, //number of ticks in x axis, calculated by default according to width
-  yTicks: undefined, //number of ticks on y axis, calculated by default according to height
-  collapsed: true,  //collapsed mode, show chart on click or mouseover
-  imperial: false    //display imperial units instead of metric
-}).addTo(myMap);
-
-gpxTrack.on("addline",function(evt){
-   hoehenProfil.addData(evt.line);
-});
-gpxTrack.addTo(myMap); 
-
-//hoehenProfil.clear();
-/*L.geoJson(geojson,{
-    onEachFeature: hoehenProfil.addData.bind(hoehenProfil) //working on a better solution
-}).addTo(myMap);
-*/
-// Zentrum der Karte setzen 
-myMap.setView([47.267,11.383],11)
+myMap.fitBounds(etappeGroups.getBounds());
 
