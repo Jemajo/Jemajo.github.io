466ec8b15949b99db534f2028a4a1ae0877cf863
marlene weiss <marlene.weiss@stud.uibk.ac.at>
2018-06-11 16:23:37 +0200

    bmap-Layer raus

--- accommodation/accommodation.js
+++ accommodation/accommodation.js
@@ -19,12 +19,6 @@ let myLayers = {
             subdomains: ["a", "b", "c"],
             attribution: "Datenquelle: <a href='https://www.openstreetmap.org'>openstreetmap.org</a>"
         }),
-    bmapoverlay: L.tileLayer(
-        "https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png",
-        {
-            subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-            attribution: "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>"
-        }),
     tiris_nomenklatur: L.tileLayer(
         "http://wmts.kartetirol.at/wmts/gdi_nomenklatur/GoogleMapsCompatible/{z}/{x}/{y}.png8", {
             attribution: "<a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>eKarte Tirol</a>",
--- kultur/kultur.js
+++ kultur/kultur.js
@@ -22,13 +22,6 @@ let myLayers = {
 
         }
     ),
-    bmapoverlay: L.tileLayer(
-        "https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png",
-        {
-            subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-            attribution: "Datenquelle: <a href='https://www.basemap.at'>basemap.at</a>"
-        }
-    ),
     tiris_nomenklatur: L.tileLayer(
         "http://wmts.kartetirol.at/wmts/gdi_nomenklatur/GoogleMapsCompatible/{z}/{x}/{y}.png8", {
             attribution: "<a href='https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol'>eKarte Tirol</a>",
